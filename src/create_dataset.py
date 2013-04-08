01bf6e340ee224e4384460ac28fc92e9de69ebe7
Andre Lynum <and-ly@online.no>
2013-04-08 23:28:27 +0200

    Indentation error.

--- src/create_dataset.py
+++ src/create_dataset.py
@@ -9,11 +9,11 @@ from treetagger import simple_tokenize, tree_tag_file
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
 
-   for fn in glob(os.path.join(nli_train_data_path, '*.txt')) \
-             + glob(os.path.join(nli_dev_data_path, '*.txt')) \
-             + glob(os.path.join(nli_test_data_path, '*.txt')):
-       simple_tokenize(fn, fn + '.tok')
-       tree_tag_file(fn + '.tok', fn + '.tok.tag')
+    for fn in glob(os.path.join(nli_train_data_path, '*.txt')) \
+            + glob(os.path.join(nli_dev_data_path, '*.txt')) \
+            + glob(os.path.join(nli_test_data_path, '*.txt')):
+        simple_tokenize(fn, fn + '.tok')
+        tree_tag_file(fn + '.tok', fn + '.tok.tag')
 
     clean_nli_data(nli_train_data_path, nli_train_dataset_fn)
     clean_nli_data(nli_dev_data_path, nli_dev_dataset_fn)
