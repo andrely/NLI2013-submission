bb9900bcf24a2d072f56d40943f94aa99efd0616
Andre Lynum <and-ly@online.no>
2013-04-12 18:08:34 +0200

    Fixed parsing of folds.

--- src/ten_fold.py
+++ src/ten_fold.py
@@ -1,21 +1,27 @@
-import logging
 from pandas import concat, pandas
-from sklearn.cross_validation import KFold
 from data import load_nli_data, load_nli_frame, nli_test_dataset_fn, nli_test_index_fn, folds_fn
+
 
 def get_folds_data():
     folds = pandas.io.parsers.read_csv(folds_fn, names=['file', 'fold', 'dataset'])
     train, dev = load_nli_data()
-    test = load_nli_frame(nli_test_dataset_fn, nli_test_index_fn)
-    data = concat((train, dev, test))
+    test_data = load_nli_frame(nli_test_dataset_fn, nli_test_index_fn)
+    data = concat((train, dev, test_data))
 
     folds_data = []
+    file_list = []
 
     for i in range(10):
-        files = folds[folds.fold == (i+1)].file
-        train = data[data.file.isin(files)]
-        test = data[-data.file.isin(files)]
+        files = folds[folds.fold == (i+1)]
+        train_files = files[files.dataset == 'train'].file
+        test_files = files[files.dataset == 'test'].file
+
+        file_list.append(sorted(train_files.values))
+
+        train = data[data.file.isin(train_files)]
+        test = data[data.file.isin(test_files)]
 
         folds_data.append((train, test))
 
     return folds_data
+
